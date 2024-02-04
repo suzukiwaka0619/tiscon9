6f2e8a780dbeae225ec0b54a0e433dc0aca51dc8
ryo kitagawa <skr.ktgw1005@gmail.com>
2024-02-04 02:07:48 +0000

    修正3

--- src/main/java/com/tiscon/dao/EstimateDao.java
+++ src/main/java/com/tiscon/dao/EstimateDao.java
@@ -90,11 +90,16 @@ public class EstimateDao {
      * @return 距離[km]
      */
     public double getDistance(String prefectureIdFrom, String prefectureIdTo) {
+        String sql;
         // 都道府県のFromとToが逆転しても同じ距離となるため、「そのままの状態のデータ」と「FromとToを逆転させたデータ」をくっつけた状態で距離を取得する。
-        String sql = "SELECT DISTANCE FROM (" +
-                "SELECT PREFECTURE_ID_FROM, PREFECTURE_ID_TO, DISTANCE FROM PREFECTURE_DISTANCE UNION ALL " +
-                "SELECT PREFECTURE_ID_TO PREFECTURE_ID_FROM ,PREFECTURE_ID_FROM PREFECTURE_ID_TO ,DISTANCE FROM PREFECTURE_DISTANCE) " +
-                "WHERE PREFECTURE_ID_FROM  = :prefectureIdFrom AND PREFECTURE_ID_TO  = :prefectureIdTo";
+        if(prefectureIdFrom.equals(prefectureIdTo)){
+            sql = "SELECT DISTANCE FROM PREFECTURE_DISTANCE WHERE PREFECTURE_ID_FROM = :prefectureIdFrom AND PREFECTURE_ID_TO  = :prefectureIdTo";
+        }else{
+            sql = "SELECT DISTANCE FROM (" +
+                    "SELECT PREFECTURE_ID_FROM, PREFECTURE_ID_TO, DISTANCE FROM PREFECTURE_DISTANCE UNION ALL " +
+                    "SELECT PREFECTURE_ID_TO PREFECTURE_ID_FROM ,PREFECTURE_ID_FROM PREFECTURE_ID_TO ,DISTANCE FROM PREFECTURE_DISTANCE) " +
+                    "WHERE PREFECTURE_ID_FROM  = :prefectureIdFrom AND PREFECTURE_ID_TO  = :prefectureIdTo";
+        }
 
         PrefectureDistance prefectureDistance = new PrefectureDistance();
         prefectureDistance.setPrefectureIdFrom(prefectureIdFrom);
